8df698e770cbde5beacd28c5ef9493c14297d6e9
Jacobo Calderon <cacovolt@gmail.com>
2016-04-15 16:18:20 +0000

    Class creation: User and Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,5 @@ class Article < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   validates :title, presence: true,
                     length: { minimum: 5 }
+  belongs_to :user  #must be singular because each article belongs to 1 user
 end
